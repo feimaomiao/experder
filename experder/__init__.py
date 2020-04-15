ba69189acfdb8db65f9ddcbb151f4ea8f344bbd1
feimaomiao <lcpmatthew@gmail.com>
2020-04-14 21:28:31 -0700

    added comments

--- experder/__init__.py
+++ experder/__init__.py
@@ -13,11 +13,14 @@ __email__ = 'lcpmatthew@gmail.com'
 def caesar_rshift(message, key=13):
     final = ''
     for char in message:
+        # Check if it goes out of range
         if (char.isupper() and ord(char) + key > 90) or (char.islower() and
                                                          ord(char) + key > 122):
             final += chr(ord(char) + key - 26)
+        # Check if it is not a character
         elif char.isalpha():
             final += chr(ord(char) + key)
+        # If the character is not an alpha char
         else:
             final += char
     return final
@@ -27,11 +30,14 @@ def caesar_rshift(message, key=13):
 def caesar_lshift(message, key=13):
     final = ''
     for char in message:
+        # Check if the number went out of range
         if (char.isupper() and ord(char) - key < 65) or (char.islower() and
                                                          ord(char) - key < 97):
             final += chr(ord(char) - key + 26)
+        # check if it is a character 
         elif char.isalpha():
             final += chr(ord(char) - key)
+        # if it is something else
         else:
             final += char
     return final
@@ -39,14 +45,18 @@ def caesar_lshift(message, key=13):
 
 # tests every possible combination for given string
 def caesar_bruteforce(message):
+    # prints shift from 1 to 25
     for i in range(26):
         print('{key:2}|{dec}'.format(dec=caesar_lshift(message, i), key=i))
 
 
 def tenkey_rshift(message, key, skip_spaces=True):
+    # Shift the message by the password
     final = ''
     key = str(key)
+    # Spaces are not counted in message
     if skip_spaces:
+        # Usage of counter to check for position in password
         counter = 0
         for char in message:
             if char.isalpha():
@@ -54,6 +64,7 @@ def tenkey_rshift(message, key, skip_spaces=True):
                 counter += 1
             else:
                 final += char
+    # Use of enumerate to check for position in password
     else:
         for count, k in enumerate(itertools.cycle(key)):
             try:
